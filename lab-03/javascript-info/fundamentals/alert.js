da85987c773636ff637930961fa37ee5de028ecf
Joe Madibekov <jmadibekov@gmail.com>
2022-02-24 01:23:15 +0600

    Add lab3

---
+++ lab-03/javascript-info/fundamentals/alert.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("I'm JavaScript");
+
+let admin, name;
+name = "John";
+admin = name;
+console.log(admin);
+
+console.log(typeof null);
+
+name = prompt("Your name?", "Don't know");
+alert(`Your name: ${name}`);
